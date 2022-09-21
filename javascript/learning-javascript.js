a0bb6bf953f28deebc612ee885fa9c3ae86e3bb6
maxi1855 <maxirodriguez1855@gmail.com>
2022-09-20 21:50:36 -0300

    first commit

---
+++ javascript/learning-javascript.js
@@ -0,0 +1,14 @@
+const CONSTANTE = 1;
+let variableVacia = null;
+let variableInicializada;
+
+/* 
+let nombre = prompt("Ingrese su nombre:");
+document.write("Bienvenido " + nombre);
+*/
+
+/*
+let nombre = "Maxi";
+let apellido = "Rodriguez";
+document.write(`Mi nombre es ${nombre} ${apellido}`);
+*/
