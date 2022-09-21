ea63c46bb36d5fc06b2e0b1c9919349810066ad6
maxi1855 <maxirodriguez1855@gmail.com>
2022-09-20 22:21:06 -0300

    javascript fue modificado

--- javascript/main.js
+++ javascript/main.js
@@ -13,3 +13,20 @@ let apellido = "Rodriguez";
 document.write(`Mi nombre es ${nombre} ${apellido}`);
 */
 
+/*
+valor1 = true;
+valor2 = false;
+document.write(valor1 && valor2);
+document.write(valor1 || valor2);
+*/
+
+/* 
+let altura = prompt("Ingrese su altura en centímetros:");
+if (altura >= 180) {
+    document.write("Usted es una persona alta");
+} else if (altura >= 170) {
+    document.write("Usted es una persona mediana");
+} else {
+    document.write("Usted es una persona baja");
+}
+*/
